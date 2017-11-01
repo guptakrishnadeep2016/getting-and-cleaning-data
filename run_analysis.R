d1299d3d5f6b3f0e12c5696f1839df8448ac8006
guptakrishnadeep2016 <guptakrishnadeep2016@gmail.com>
2017-11-02 00:51:25 +0530

    Commit

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
 
-# Unzip dataSet to /data directory
+# Unzip dataSet to  /data directory
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 # Reading trainings tables:
 x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
